8d5b2dccdcde075195737ea53c490cf5658748f1
AlFalah <taiebalfalah@gmail.com>
2019-03-11 22:38:13 +0100

    clean the code

--- index.py
+++ index.py
@@ -5,38 +5,48 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-def do_work():
-    """ Function to handle command line usage"""
-    args = sys.argv
-    args = args[1:]  # First element of args is the file name
 
-    if len(args) == 0:
-        print('You have not passed any commands in!')
+
+def do_work():
+        
+    operation = input('''
+    Please type in the math operation you would like to complete:
+            facebook to connect to facebook
+            gmail to connect to gmail
+            hattrick to connect to hattrick
+            reddit to connect to reddit
+    ''')
+
+    if operation == 'facebook':
+        driver = webdriver.Firefox()
+        driver.get("https://www.facebook.com")
+        # assert "facebook" in driver.title
+        elem = driver.find_element_by_id("email")
+        elem.clear()
+        elem.send_keys(secrets.email)
+        elem = driver.find_element_by_id("pass")
+        elem.clear()
+        elem.send_keys(secrets.pwd)
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+    elif operation =='gmail':
+        driver = webdriver.Firefox()
+        driver.get("https://www.gmail.com")
+        # assert "facebook" in driver.title
+        elem = driver.find_element_by_id("identifierId")
+        elem.clear()
+        elem.send_keys(secrets.email)
+        elem = driver.find_element_by_id("pass")
+        elem.clear()
+        elem.send_keys(secrets.pwd)
+        elem.send_keys(Keys.RETURN)
     else:
-        for a in args:
-            if a == '--help':
-                print('Basic command line program')
-                print('Options:')
-                print('    --help -> show this basic help menu.')
-                print('    --facebook -> facebook.')
-                print('    --veg -> show a random vegetable')
-            elif a == '--facebook':
-                driver = webdriver.Firefox()
-                driver.get("https://www.facebook.com")
-                # assert "facebook" in driver.title
-                elem = driver.find_element_by_id("email")
-                elem.clear()
-                elem.send_keys(secrets.email)
-                elem = driver.find_element_by_id("pass")
-                elem.clear()
-                elem.send_keys(secrets.pwd)
-                elem.send_keys(Keys.RETURN)
-                assert "No results found." not in driver.page_source
-                # driver.close()
-            elif a == '--veg':
-                print(random.choice(['Carrot', 'Potato', 'Turnip']))
-            else:
-                print('Unrecognised argument.')
+        print('Unrecognised argument.')
+
+
+
+
+
 
 if __name__ == '__main__':
     do_work()
